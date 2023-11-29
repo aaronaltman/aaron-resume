3d53e6ae9dd2d9b77575c05afa349b7d297ceed6
aka <aaronkaltman@gmail.com>
2023-11-28 22:58:50 -0500

    primary menu

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Aaron K. Altman</h1>
+      <h1 className="text-center">Aaron K. Altman</h1>
       <h2>Software Engineer</h2>
     </>
   );
---
+++ components/global/navigation/primaryNavigation.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const PrimaryNavigation = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
